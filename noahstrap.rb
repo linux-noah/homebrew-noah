dd0404d0b003d7dc99b3ed4fc43dad045e58be69
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2016-09-26 08:36:31 +0900

    add noahstrap 0.0.2

---
+++ noahstrap.rb
@@ -0,0 +1,30 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Noahstrap < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/linux-noah/noahstrap/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "a70db763ad0b9c8901d3641bb674b3c9a27f866d498cead434629f233da88035"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    system "make", "install", "PREFIX=#{prefix}" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test noahstrap`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
