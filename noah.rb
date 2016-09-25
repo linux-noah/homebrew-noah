d77463746e4d4039d5894601a4b5298873f4565f
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2016-09-26 05:23:14 +0900

    version 0.2.0

---
+++ noah.rb
@@ -0,0 +1,34 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Noah < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/linux-noah/noah/archive/0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "e09bef1e519880f6f110c0c4a4b5be5a170f48da5ce79a04fc478f6c96a38f05"
+
+  # depends_on "cmake" => :build
+  #depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    system "make"
+    bin.install "build/noah"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test noah`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
