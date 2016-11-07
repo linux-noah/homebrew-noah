4a131865d8c91342afde0f23ee6c9b6be82bf2d9
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2016-11-07 16:24:18 +0900

    cleanup noah.rb

--- noah.rb
+++ noah.rb
@@ -1,10 +1,6 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Noah < Formula
-  desc ""
-  homepage ""
+  desc "Darwin subsystem for Linux (Bash on macOS)"
+  homepage "http://github.com/linux-noah/noah"
   url "https://github.com/linux-noah/noah/archive/0.3.0.tar.gz"
   version "0.3.0"
   sha256 "fa026e86ede1d3fcdb395afc89c5a53cefa5db952aa3c751805a6918ae6c5330"
@@ -12,23 +8,25 @@ class Noah < Formula
   depends_on "noahstrap"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
     system "make"
+
     bin.install "bin/noah"
     libexec.install "libexec/noah"
   end
 
+  def caveats
+    s = <<-EOS.undent
+      To allow the sudo command to run inside the linux land, #{name} requires
+      superuser priviledges. You must change the ownership of the binary
+      and add the setuid bit.
+
+        sudo chown root:admin #{libexec}/noah
+        sudo chmod u+s #{libexec}/noah
+    EOS
+    s
+  end
+
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test noah`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
